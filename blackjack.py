680eee1fd1d83ed406f610cea7bd2bc0244c718b
Kyle <khernandez93@optonline.net>
2020-12-11 18:16:41 -0500

    Initial Commit

---
+++ blackjack.py
@@ -0,0 +1,191 @@
+from gameObjects import *
+
+auto_config = True
+min_bet_size = 50
+if auto_config:
+    players = [Player('Kyle', 1000), Player('John', 1000), Player('Randy', 1000)]
+else:
+    name = input('Enter player name: ')
+    while True:
+        try:
+            money = int(input(f'How much is {name} sitting down with (min bet size of {min_bet_size}): '))
+            break
+        except:
+            print("Must enter a number!")
+    players = [Player(name, money)]
+    add_more = input('Would you like to add more players [Y/N]: ')
+    while add_more in 'Yy' and Player.num_of_players <= 6:
+        name = input('Enter player name: ')
+        while True:
+            try:
+                money = int(input(f'How much is {name} sitting down with (min bet size of {min_bet_size}): '))
+                break
+            except:
+                print("Must enter a number!")
+        players.append(Player(name, money))
+        if Player.num_of_players == 6:
+            print('Table is full...starting the game')
+            break
+        add_more = input('Would you like to add more players [Y/N]: ')
+still_playing = True
+
+# Game begins
+while True:
+    while still_playing:
+        # initialize the game
+        dealer = Dealer()
+        deck = Deck()
+        deck.shuffle()
+        continuing_players = players.copy()
+
+        # display how much money each players has
+        for i in range(len(players)):
+            print(f'{players[i].name} has ${players[i].money}')
+        print('')
+
+        # initial round of betting before the hand
+        for i in range(len(players)):
+            input_not_valid = True
+            while input_not_valid:
+                if players[i].money == 0:
+                    print(f'{players[i].name} has no money and needs to reload')
+                    players[i].reload(min_amount=min_bet_size)
+                bet_amount = input(f'{players[i].name}, enter betting amount: ')
+                try:
+                    bet_amount = int(bet_amount)
+                    input_not_valid = False
+                    if players[i].money < bet_amount:
+                        print(f'{players[i].name} does not have that much money')
+                        input_not_valid = True
+                except:
+                    print('Need to enter a number')
+            players[i].bet(bet_amount)
+
+        dealer.deal_starting_hand(players, deck)
+
+        # check the starting hands for black jack and pay winning players + display starting hands
+        for i in range(len(players)):
+            players[i].display_current_hand()
+            if players[i].blackjack_check(dealer):
+                continuing_players.remove(players[i])
+        if len(continuing_players) == 0:
+            break
+
+        # show the dealers hand and check for blackjack if necessary
+        dealer.display_hand(starting=True)
+        if dealer.hand.cards[0].value >= 10 or dealer.hand.cards[0].value == 1:
+            if dealer.blackjack_check():
+                for i in range(len(players)):
+                    players[i].current_bet = 0
+                break
+
+        # Players begin playing their hands
+        for i in range(len(continuing_players)):
+            while True:
+                continuing_players[i].display_current_hand()
+                option = continuing_players[i].option()
+                if option == 'h':
+                    bust = continuing_players[i].hit(deck)
+                    if bust:
+                        continuing_players[i].display_current_hand()
+                        print("Bust! You lost the hand")
+                        dealer.remove_player_hand(continuing_players[i])
+                        break
+                if option == 'd':
+                    bust = continuing_players[i].double_down(deck)
+                    continuing_players[i].display_current_hand()
+                    if bust:
+                        print("Why would you ever double down with a chance to bust?? You lost that hand")
+                        dealer.remove_player_hand(continuing_players[i])
+                    break
+                if option == 's':
+                    continuing_players[i].stand()
+                    break
+                if option == 'p':
+                    current_index = 0
+                    continuing_players[i].split(deck)
+                    while True:
+                        continuing_players[i].display_current_hand(current_index)
+                        if continuing_players[i].hands[current_index].cards[0].value == 1:  # if splitting aces, you get one card and done
+                            continuing_players[i].display_current_hand(current_index+1)
+                            break
+                        option = continuing_players[i].option(current_index)
+                        if option == 'h':
+                            bust = continuing_players[i].hit(deck, current_index)
+                            if bust:
+                                continuing_players[i].display_current_hand(current_index)
+                                # continuing_players[i].current_bet -= continuing_players[i].initial_bet
+                                print("Bust!  Lost that hand")
+                                dealer.remove_player_hand(continuing_players[i], current_index)
+                        if option == 'p':
+                            continuing_players[i].split(deck, current_index)
+                        if option == 's':
+                            continuing_players[i].stand(current_index)
+                            current_index += 1
+                        if option == 'd':
+                            bust = continuing_players[i].double_down(deck, current_index)
+                            continuing_players[i].display_current_hand(current_index)
+                            if bust:
+                                print("Why would you ever double down with a chance to bust?? You lost that hand")
+                                dealer.remove_player_hand(continuing_players[i], current_index)
+                                # continuing_players[i].current_bet -= continuing_players[i].initial_bet
+                                current_index -= 1
+                            current_index += 1
+                            if current_index == len(continuing_players[i].hands):
+                                break
+                        if current_index > len(continuing_players[i].hands)-1:
+                            break
+                    break
+
+        # check if everyone busted.  if so, the dealer does not have to draw cards
+        count = 0
+        for i in range(len(continuing_players)):
+            if len(continuing_players[i].hands) == 0:
+                count += 1
+        if count == len(continuing_players):
+            print("all players busted.")
+            break
+
+        # the hands have been played.  Time for the Dealer to draw cards
+        dealer_bust = dealer.dealer_turn(deck)
+
+        # Compare the dealers hand score to the scores of the remaining player hands
+        for i in range(len(continuing_players)):
+            wins = 0
+            push = 0
+            for j in range(len(continuing_players[i].hands)):
+                if type(continuing_players[i].hands[j].score()) is list:
+                    current_player_score = max(continuing_players[i].hands[j].score())
+                else:
+                    current_player_score = continuing_players[i].hands[j].score()
+                if dealer_bust or dealer.get_score() < current_player_score:
+                    if dealer_bust:
+                        wins += 1
+                        dealer.payout(continuing_players[i], j)
+                    else:
+                        wins += 1
+                        continuing_players[i].display_current_hand(j)
+                        print(f"{continuing_players[i].name} wins that hand")
+                        dealer.payout(continuing_players[i], j)
+                elif dealer.get_score() > current_player_score:
+                    continuing_players[i].display_current_hand(j)
+                    print(f"{continuing_players[i].name} lost that hand")
+                else:
+                    continuing_players[i].display_current_hand(j)
+                    dealer.payout(continuing_players[i], blackjack=False, push=True)
+                    print("Push!")
+            continuing_players[i].current_bet = 0
+        break
+    play_again = input('The hand is over. Play another hand? [Y/N]')
+    if play_again in 'Yy':
+        temp_index = 0
+        if len(continuing_players) > 0:
+            for i in range(len(continuing_players)):
+                ind = players.index(continuing_players[i])
+                players[ind] = continuing_players[i]
+            del continuing_players
+            print('\n\n')
+    else:
+        still_playing = False
+        break
+
---
+++ gameObjects.py
@@ -0,0 +1,274 @@
+from random import shuffle
+
+
+class Card(object):
+    def __init__(self, value, suit, is_visible=True):
+        points_lookup = {1: (1, 11), 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 10, 12: 10, 13: 10}
+        self.value = value
+        self.suit = suit
+        self.points = points_lookup[value]
+        self.is_visible = is_visible
+
+    def get_value(self):
+        table = {1: 'Ace', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+                 11: 'Jack', 12: 'Queen', 13: 'King'}
+        return table[self.value]
+
+    def get_suit(self):
+        table = {0: 'Clubs', 1: 'Diamonds', 2: 'Hearts', 3: 'Spades'}
+        return table[self.suit]
+
+    def show(self):
+        if self.is_visible:
+            print(self.get_value(), 'of', self.get_suit(), end='')
+        else:
+            print('<Face Down Card>', end='')
+
+
+class Hand(object):
+    def __init__(self, cards):
+        self.cards = cards
+        if len(self.cards) == 2:
+            if self.cards[0].value == self.cards[1].value:
+                self.is_pair = True
+            else:
+                self.is_pair = False
+        else:
+            self.is_pair = False
+        self.is_complete = False
+        self.a_double = False
+
+    def score(self):
+        score = [0, 0]  # initialize the score as a list to deal with aces easier
+        for i in range(len(self.cards)):
+            if self.cards[i].value == 1:  # if the card is an ace, need to account for 1 or 11 points
+                temp = score.copy()
+                score[0] = min(temp) + self.cards[i].points[0]
+                score[1] = min(temp) + self.cards[i].points[1]
+            else:
+                score[0] += self.cards[i].points
+                score[1] += self.cards[i].points
+        if len(set(score)) == 1 or score[1] > 21:
+            return score[0]
+        else:
+            return score
+
+    def split(self):
+        hand1 = Hand([self.cards[0]])
+        hand2 = Hand([self.cards[1]])
+        return [hand1, hand2]
+
+    def show(self, dealer_starting_hand=False):
+        for i in range(len(self.cards)):
+            self.cards[i].show()
+            if i != len(self.cards) - 1:
+                print(', ', end='')
+            else:
+                if not dealer_starting_hand:
+                    if type(self.score()) is list:
+                        print(f'   <{self.score()[0]} or {self.score()[1]}>')
+                    else:
+                        print(f'   <{self.score()}>')
+                else:
+                    print('')
+
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        for value in range(1, 14):
+            for suit in range(4):
+                self.cards.append(Card(value, suit))
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def deal(self):
+        return self.cards.pop()
+
+
+class Player(object):
+    num_of_players = 0
+
+    def __init__(self, name, money, hands=[], initial_bet=0, current_bet=0):
+        self.name = name
+        self.hands = hands
+        self.money = money
+        self.current_bet = current_bet
+        self.initial_bet = initial_bet
+        Player.add_player()
+
+    def bet(self, amount):
+        self.current_bet = 0
+        if amount > self.money:
+            print(f'{self.name} needs to reload')
+            self.reload()
+        else:
+            self.money -= amount
+            self.current_bet += amount
+            self.initial_bet = amount
+
+    def reload(self, min_amount=0):
+        input_not_valid = True
+        while input_not_valid:
+            amount = input(f'{self.name}, total money: {self.money}, money to add: ')
+            try:
+                amount = int(amount)
+                input_not_valid = False
+                if amount < min_amount:
+                    print(f'You need to add a minimum of {min_amount}')
+                    input_not_valid = True
+            except:
+                print('Need to enter a number')
+        self.money += amount
+
+    @classmethod
+    def add_player(cls):
+        cls.num_of_players += 1
+
+    def display_current_hand(self, hand=0):
+        print(f"{self.name}'s current hand: ", end='')
+        self.hands[hand].show()
+
+    def blackjack_check(self, dealer):
+        if type(self.hands[0].score()) is list:
+            if max(self.hands[0].score()) == 21:
+                print(f"{self.name} wins! paying out...")
+                dealer.payout(self, blackjack=True)
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def option(self, index=0):
+        choice = ' '
+        if self.hands[index].is_pair:
+            print(f"{self.name}: [H]it, [S]tand, [D]ouble-down, or S[p]lit")
+            while choice not in 'HhSsDdPp':
+                choice = input('choice: ').lower()
+        else:
+            print(f"{self.name}: [H]it, [S]tand, or [D]ouble-down")
+            while choice not in 'HhSsDd':
+                choice = input('choice: ').lower()
+        return choice
+
+    def hit(self, deck, index=0):
+        self.hands[index].cards.append(deck.deal())
+        if self.hands[index].cards[0].value == self.hands[index].cards[1].value:
+            self.hands[index].is_pair = True
+        if type(self.hands[index].score()) is list:
+            return False
+        else:
+            if self.hands[index].score() > 21:
+                return True
+            else:
+                return False
+
+    def double_down(self, deck, index=0):
+        if self.money < self.initial_bet:
+            self.reload(abs(self.money-self.initial_bet))
+        self.current_bet += self.initial_bet
+        self.money -= self.initial_bet
+        self.hands[index].a_double = True
+        return self.hit(deck, index)
+
+    def split(self, deck, index=0):
+        if self.money < self.initial_bet:
+            self.reload(abs(self.money-self.initial_bet))
+        # self.current_bet += self.initial_bet
+        self.money -= self.initial_bet
+        split_hands = self.hands[index].split()
+        self.hands.remove(self.hands[index])
+        self.hands.insert(index, split_hands[0])
+        self.hands.insert(index+1, split_hands[1])
+        self.hit(deck, index)
+        self.hit(deck, index+1)
+
+    def stand(self, index=0):
+        self.hands[index].is_complete = True
+
+
+class Dealer(object):
+    def __init__(self, hand=[]):
+        self.hand = hand
+
+    def deal_starting_hand(self, players, deck):
+        temp_list = []
+        dealer_hand = []
+        for i in range(Player.num_of_players):
+            temp_list.append([])
+        while len(dealer_hand) < 2:
+            for i in range(len(players)):
+                temp_list[i].append(deck.deal())
+            dealer_hand.append(deck.deal())
+        dealer_hand[1].is_visible = False
+        self.hand = Hand(dealer_hand)
+        for i in range(len(players)):
+            temp_hand = Hand(temp_list[i])
+            players[i].hands = [temp_hand]
+
+    def blackjack_check(self):
+        print('Dealer is checking for blackjack...')
+        if type(self.hand.score()) is list:
+            if max(self.hand.score()) == 21:
+                print('Dealer has blackjack. You LOSE!')
+                self.hand.cards[1].is_visible = True
+                self.display_hand()
+                return True
+            else:
+                print('Dealer does not have blackjack')
+                return False
+        else:
+            print('Dealer does not have blackjack')
+            return False
+
+    @staticmethod
+    def remove_player_hand(player, index=0):
+        player.hands.remove(player.hands[index])
+        # player.current_bet -= player.initial_bet
+
+    def display_hand(self, starting=False):
+        if starting:
+            print('')
+        print("Dealer's hand: ", end='')
+        self.hand.show(starting)
+        if starting:
+            print('')
+
+    @staticmethod
+    def payout(player, index=0, blackjack=False, push=False):
+        if blackjack:
+            player.money += player.current_bet * (5/2)
+        elif player.hands[index].a_double:
+            if push:
+                player.money += player.initial_bet * 2
+            else:
+                player.money += player.initial_bet * 4
+        else:
+            if push:
+                player.money += player.initial_bet
+            else:
+                player.money += player.initial_bet * 2
+
+    def dealer_turn(self, deck):
+        bust = False
+        self.hand.cards[1].is_visible = True
+        self.display_hand()
+        while self.get_score() < 17:
+            self.hand.cards.append(deck.deal())
+            self.display_hand()
+            if self.get_score() > 21:
+                bust = True
+                print("Dealer busted, paying out winning players")
+                return bust
+            else:
+                bust = False
+        print(f"Dealer stands at {self.get_score()}")
+        return bust
+
+    def get_score(self):
+        if type(self.hand.score()) is list:
+            return max(self.hand.score())
+        else:
+            return self.hand.score()
